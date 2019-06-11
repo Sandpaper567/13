97db3b52a7df7b2d83b5f4c3fbb085a8c1a96941
Sahaj Patel <patelsahajemail@gmail.com>
2019-06-11 12:06:15 -0400

    Combined all work into one single file

--- Runner.java
+++ Runner.java
@@ -3,10 +3,15 @@ interface Square{
     boolean isSquare(int x);
  }
 public class Runner{
-    public static void main(String[] args){
+  interface printQuotient{
+      void printQuotientOfNums(float n1 ,float n2);
+  }
+  public static void main(String[] args){
         Square theSquare = (int x) -> {
 									return (Math.pow((int)Math.sqrt(x),2) == x);
 								};
         System.out.println(theSquare.isSquare(9));
+        printQuotient divide = (float num1, float num2) -> System.out.printf("Quotient is: %.3f\n", num1/num2);
+        divide.printQuotientOfNums(3.0f, 2.0f);
     }
 }
