5d6f6dc83295f93edb7cc7f4e274a290e5fb2001
Sahaj Patel <patelsahajemail@gmail.com>
2019-06-11 12:08:13 -0400

    Partner B added comments about what he/she learned

--- Runner.java
+++ Runner.java
@@ -14,4 +14,4 @@ public class Runner{
         printQuotient divide = (float num1, float num2) -> System.out.printf("Quotient is: %.3f\n", num1/num2);
         divide.printQuotientOfNums(3.0f, 2.0f);
     }
-}
+}//Hey there Mr.Schiff! I learned that 1, it takes 13 attempts to get this right, 2, how to use a lambda funtion (which is so much better in python btw), and finally, how to use the Sout.printf() to format for a decimal
